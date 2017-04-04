61cff3b2fa40149d24398d8f80b62189b1951d8c
Basim Hennawi <basim.a.hennawi@gmail.com>
2017-04-04 20:07:33 +0200

    IMPLEMENT: react-star-rating-component (closes #18)

--- client/components/RecordingsList/index.js
+++ client/components/RecordingsList/index.js
@@ -4,6 +4,7 @@ import { push } from 'react-router-redux';
 import { connect } from 'react-redux';
 import { injectIntl } from 'react-intl';
 import ReactAudioPlayer from 'react-audio-player';
+import StarRatingComponent from 'react-star-rating-component';
 import Loading from 'components/Loading';
 import { formatDate, convertSecsToMins } from 'utils/helpers';
 import { fetchRecordings } from './actions';
@@ -31,6 +32,23 @@ class RecordingsList extends React.Component { // eslint-disable-line react/pref
     }
   }
 
+  formatFinalScript(str) {
+    var urlRegex = 
+      new RegExp(
+      '((https?://)' + 
+      '(([0-9]{1,3}\.){3}[0-9]{1,3}' + // IP- 199.194.52.184 
+      '|' + // allows either IP or domain 
+      '([0-9a-z_!~*\'()-]+\.)*' + // tertiary domain(s)- www. 
+      '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\.' + // second level domain 
+      '[a-z]{2,6})' + // first level domain- .com or .museum 
+      '(:[0-9]{1,4})?' + // port number- :80 
+      '((/?)|' + // a slash isn't required if there is no file name 
+      '(/[0-9a-z_!~*\'().;?:@&=+$,%#-]+)+/?))',
+      "gi");
+
+    return str.replace(urlRegex, "<a href='$1'>$1</a>").replace(/(?:\r\n|\r|\n)/g, '<br/>');
+  }
+
   render() {
     const {
       status,
@@ -40,22 +58,120 @@ class RecordingsList extends React.Component { // eslint-disable-line react/pref
     if (status.isLoading) {
       return <Loading />;
     }
+    
 
     const PageWrapper = styled.section`
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
     `;
+
+    const Title = styled.h1`
+      font-size: 30px;
+      text-align: center;
+      margin: 20px 0;
+    `;
+
+    const RecordingWrapper = styled.div `
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 20px;
+      border: 1px solid #aaa;
+      padding: 20px;
+      @media (max-width: 768px) {
+        padding: 10px;
+      }
+
+    `;
+
+    const FinalScript = styled.p`
+      line-height: 125%;
+      margin-bottom: 10px;
+      font-size: 16px;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 20px;
+
+      & a {
+        text-decoration: none;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: inline-block;
+        width: auto;
+        margin-bottom: -20px;
+        @media (max-width: 768px) {
+          font-size: 14px;
+        }
+      }
+    `;
+
+    const Rating = styled.div `
+      display: flex;
+    `
+
+    const AudioDuration = styled.h4 `
+      line-height: 120%;
+      display: flex;
+      font-size: 16px;
+      & * {
+        font-weight: 500;
+      }
+      & h4 {
+        font-size: 16px;
+        margin-right: 5px;
+        flex-shrink: 0;
+      }      
+      & p {
+        font-size: 14px;
+      }
+    `
+
+    const AudioRecording = styled.div `
+      line-height: 120%;
+      display: flex;
+      align-items: center;
+    `
+
+    const CreationDate = styled.div `
+      display: flex;
+      align-items: center;
+      & h4 {
+        font-weight: 500;
+        font-size: 16px;
+        margin-right: 5px;
+        flex-shrink: 0;
+      }
+      & p {
+        font-size: 14px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    `
 
     return (
       <PageWrapper>
-        <h3>RecordingsList component</h3>
+        <Title>Recordings List</Title>
         {list.map((item, index) => (
-          <div key={index}>
-            Text: <h4>{item.final_script}</h4>
-            Rating: <h4>{item.rating}</h4>
-            Duration: <h4>{convertSecsToMins(item.duration)}m</h4>
-            Audio URL: <ReactAudioPlayer src={item.url} />
-            Created at: <h4>{formatDate(item.created, 'MMMM DD, YYYY, h:mm a')}</h4>
-            <hr />
-          </div>
+          <RecordingWrapper key={index}>
+            <FinalScript dangerouslySetInnerHTML={{__html: this.formatFinalScript(item.final_script)}}/>
+            <Rating>
+              <StarRatingComponent 
+                name={index} 
+                editing={false}
+                renderStarIcon={() => <span>★</span>}
+                starCount={5}
+                value={item.rating}
+              />
+            </Rating>
+            <AudioDuration><h4>Audio duration:</h4><p title={convertSecsToMins(item.duration)+"m"}>{convertSecsToMins(item.duration)}m</p></AudioDuration>
+            <AudioRecording>
+              <ReactAudioPlayer src={item.url} />
+            </AudioRecording>
+            <CreationDate><h4>Created at:</h4><p title={formatDate(item.created, 'MMMM DD, YYYY, h:mm a')}>{formatDate(item.created, 'MMMM DD, YYYY, h:mm a')}</p></CreationDate>
+          </RecordingWrapper>
         ))}
       </PageWrapper>
     );
